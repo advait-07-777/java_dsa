4d942aa252d2ea8160f931d964816f94eaa2f07a
advait-07-777 <advaitjagtap07@gmail.com>
2026-03-20 18:00:22 +0530

    Create solid_rectangle.java

---
+++ Patterns/Basic/solid_rectangle.java
@@ -0,0 +1,13 @@
+public class patterns {
+    public static void main(String[] args){
+        //Pattern 1 solid rectangle
+        int n = 4;
+        int m = 5;
+        for (int i=1 ; i <= n ; i++){
+            for (int j=1 ; j <= m ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
