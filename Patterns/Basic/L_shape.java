312fa6a6ab84892504043ddd9f54ed0504e3915e
advait-07-777 <advaitjagtap07@gmail.com>
2026-03-20 18:02:41 +0530

    Create L_shape.java

---
+++ Patterns/Basic/L_shape.java
@@ -0,0 +1,16 @@
+public class patterns {
+    public static void main(String[] args){
+        int i, j;
+        for (i = 1; i <= 5; i++){
+            for (j = 1; j<= 5; j++){
+                if(j == 1 || i == 5){
+                    System.out.print("*");
+                }
+               else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+}
