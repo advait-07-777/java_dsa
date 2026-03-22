ee41d340b45d9d0b3d3d765a8940bbcf0b879c90
advait-07-777 <advaitjagtap07@gmail.com>
2026-03-22 23:40:10 +0530

    Create right_aligned_number_triangle..java

---
+++ Patterns/Basic/right_aligned_number_triangle..java
@@ -0,0 +1,15 @@
+public class patterns {
+    public static void main(String[] args){
+        int i,j;
+        for (i=1; i<=5 ; i++) {
+            for (j = 1; j<=5 ; j++){
+                if (i <= j) {
+                    System.out.print(j);
+                }  else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+       }
+    }
+}
