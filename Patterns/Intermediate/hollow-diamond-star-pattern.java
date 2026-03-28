4a67a974783b774af82d999b788a20aa8c63bdaa
advait-07-777 <advaitjagtap07@gmail.com>
2026-03-29 01:15:20 +0530

    Create hollow-diamond-star-pattern.java

---
+++ Patterns/Intermediate/hollow-diamond-star-pattern.java
@@ -0,0 +1,17 @@
+public class patterns {
+    public static void main(String[] args){
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                if (j <= 6 - i || j >= 4 + i){
+                    System.out.print("*");
+                } else if (j <= i - 4 || j >= 14-i && i>5) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
