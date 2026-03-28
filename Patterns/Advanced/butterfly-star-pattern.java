5b14b9ee20abbdff5e693a445ef0df55c80825b8
advait-07-777 <advaitjagtap07@gmail.com>
2026-03-29 02:27:32 +0530

    Create butterfly-star-pattern.java

---
+++ Patterns/Advanced/butterfly-star-pattern.java
@@ -0,0 +1,16 @@
+public class patterns {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                if ((j <= i || j >= 10 - i) && i <= 5) {
+                    System.out.print("*"+" ");
+                } else if ((j <= 10 - i || j >= i) && i > 5) {
+                    System.out.print("*"+" ");
+                } else {
+                    System.out.print(" "+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
