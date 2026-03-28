4ec33aa33b0715a4989197e3f6fecaf17da6d317
advait-07-777 <advaitjagtap07@gmail.com>
2026-03-28 21:02:04 +0530

    Create Half_Diamond_Star_Pattern.java

---
+++ Patterns/Intermediate/Half_Diamond_Star_Pattern.java
@@ -0,0 +1,15 @@
+public class patterns {
+    public static void main(String[] args){
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 5; j++){
+                if (i >= j && i <=5 || j <= 10 - i && i > 5){
+                    System.out.print("*" + " ");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+}
