9d0b68037aea2546d07d2ab0a23b647009fe3397
advait-07-777 <advaitjagtap07@gmail.com>
2026-04-05 01:49:01 +0530

    Create Array_4_print_even_values.java

---
+++ Array/Array_4_print_even_values.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class array{
+    public static void main(String[] args){
+        int a [] = new int [5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 5 values in array :");
+        for (int i = 0; i < a.length; i++){
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] % 2 == 0){
+                System.out.println("The even values are : "+ a[i]);
+            }
+        }
+
+    }
+}
